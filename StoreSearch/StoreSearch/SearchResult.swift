e8bc92ef9aad748e72222b8f51281f24c55663d1
kopparthi sai suman <ksaisuman@kunals-mac-mini.lan>
2017-08-07 13:32:56 +0530

    Add a search bar and table view. The search puts fake results in the table for now.

---
+++ StoreSearch/StoreSearch/SearchResult.swift
@@ -0,0 +1,12 @@
+//
+//  SearchResult.swift
+//  StoreSearch
+//
+//  Created by kopparthi sai suman on 07/08/17.
+//  Copyright © 2017 kopparthi sai suman. All rights reserved.
+//
+
+class SearchResult {
+    var name = ""
+    var artistName = ""
+}
