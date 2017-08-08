d7fe5cb59dd3f5e4daa44b1f22b515a3e77bccc6
kopparthi sai suman <ksaisuman@kunals-mac-mini.lan>
2017-08-08 18:29:53 +0530

    added networking

--- StoreSearch/StoreSearch/SearchResult.swift
+++ StoreSearch/StoreSearch/SearchResult.swift
@@ -6,7 +6,20 @@
 //  Copyright © 2017 kopparthi sai suman. All rights reserved.
 //
 
+func < (lhs: SearchResult, rhs: SearchResult) -> Bool {
+    return lhs.name.localizedStandardCompare(rhs.name) == .orderedAscending
+}
+
 class SearchResult {
     var name = ""
     var artistName = ""
+    var artworkSmallURL = ""
+    var artworkLargeURL = ""
+    var storeURL = ""
+    var kind = ""
+    var currency = ""
+    var price = 0.0
+    var genre = ""
 }
+
+
